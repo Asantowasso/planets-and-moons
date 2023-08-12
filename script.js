c71838b96657c3bd5817f309d43f689e01333be3
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-08-12 12:02:25 -0400

    adding information from the API to the HTML page

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ fetch("https://api.le-systeme-solaire.net/rest/bodies/ ")
   .then(function (obj){
     console.log(obj)
     console.log(obj.bodies[0].englishName) // info for earth's moon
+
     console.log(obj.bodies[0].perihelion) //closest distance to the sun
     console.log(obj.bodies[0].aphelion) //farthest distance to the sun
+
+     var name1 = obj.bodies[0].englishName;
+    document.querySelector(".section1").innerHTML = name1;
 
   })
